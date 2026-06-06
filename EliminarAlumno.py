968ccd229b6ffa900e6a69f94898118da3a54002
valeriarios1502 <valeria.rios@utec.edu.pe>
2026-06-05 20:39:52 -0500

    eliminar

--- EliminarAlumno.py
+++ EliminarAlumno.py
@@ -2,8 +2,8 @@ import boto3
 
 def lambda_handler(event, context):
     # Entrada (json)
-    tenant_id = event['tenant_id']
-    alumno_id = event['alumno_id']
+    tenant_id = event['body']['tenant_id']
+    alumno_id = event['body']['alumno_id']
     # Proceso
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('t_alumnos')
